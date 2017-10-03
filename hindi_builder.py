58683c751b099e23241220b16615f128ceb81bfd
Himanshu Attri <attri.him@gmail.com>
2017-10-03 07:42:16 +0530

    few changes
    translation, submit on press enter,

--- hindi_builder.py
+++ hindi_builder.py
@@ -5,7 +5,8 @@ import numpy as np
 import os.path
 
 root = Tk()
-root.title("Hindi_Dictonary_Buil")
+root.tk.call('encoding', 'system', 'utf-8')
+root.title("Hindi_Dictonary_Builder")
 isav=0
 reload(sys)  
 sys.setdefaultencoding('utf-8')
@@ -25,15 +26,21 @@ def isa():
 
   
   
+def en(x):
+  
+    
+   e.bind('<Return>', read)
 
 
 
-def add():
+
+
+def add(x):
     global isav
     if len(e.get())==0 or len(f.get())==0:
      err=Tk()
-     Label(err, text="Error: Invalid Entry",font=(None, sz)).grid(row=0,column=0)
-     Button(err, text='Ok',command=err.destroy,font=(None, sz)).grid(row=1,column=0)
+     Label(err, text="स्खलन: अमान्य प्रविष्टि ",font=(None, sz)).grid(row=0,column=0)
+     Button(err, text='ठीक',command=err.destroy,font=(None, sz)).grid(row=1,column=0)
 
      print "Error himanshu"
      return 0
@@ -45,6 +52,9 @@ def add():
       Label(err, text="शब्दकोश में शब्द पहले से मौजूद है\n",font=(None, sz)).grid(row=0,column=0)
       Button(err, text='ठीक',command=err.destroy,font=(None, sz)).grid(row=1,column=0)
       return 0
+
+    
+
   
    
     F = open(e.get(),'w') 
@@ -53,14 +63,21 @@ def add():
     e.delete(0,'end')
    
 
-def read():
+def read(x):
+    f.delete(0,'end')
     T.delete('1.0', END)
     if len(e.get())==0:
+     err.geometry('10x10+0+0') 
      err=Tk()
      Label(err, text="Error: Invalid Entry",font=(None, sz)).grid(row=0,column=0)
      Button(err, text='Ok',command=err.destroy,font=(None, sz)).grid(row=1,column=0)
      print "Error himanshu"
      return 0
+    if os.path.exists(e.get())==False:
+           err=Tk()
+           Label(err, text="शब्द शब्दकोश में नहीं है",font=(None, sz)).grid(row=0,column=0)
+           Button(err, text='ठीक',command=err.destroy,font=(None, sz)).grid(row=1,column=0)
+
     F = open(e.get(),'r') 
     st=F.read()
     f.insert(0,st)
@@ -98,15 +115,16 @@ def listall():
       G = open('final.txt','a')
       G.write(word+": "+F.read()+"\n")
 
-Label(root, text="शब्द",font=(None, sz)).grid(row=0)
-Label(root, text="अर्थ",font=(None, sz)).grid(row=1)
+Label(root, text="शब्द",font=(None, sz)).grid(row=1)
+Label(root, text="अर्थ",font=(None, sz)).grid(row=2)
 e = Entry(root,font=(None, sz))
 f = Entry(root,font=(None, sz))
 
-e.grid(row=0, column=1)
 
-f.grid(row=1, column=1)
-status=Label(root, text="",font=(None, sz)).grid(row=0,column=2)
+e.grid(row=1, column=1)
+
+f.grid(row=2, column=1)
+status=Label(root, text="",font=(None, sz)).grid(row=1,column=2)
 
 
 
@@ -115,16 +133,23 @@ status=Label(root, text="",font=(None, sz)).grid(row=0,column=2)
 Button(root, text='Save', command=add)
 
 T = Text(root, height=20, width=30,font=(None, sz))
-T.grid(row=3,column=1)
+T.grid(row=4,column=1)
+Label(root, text="देवनागरी शब्दकोश संपादक",font=(None, 15)).grid(row=0,column=1)
 
 
-Button(root, text='खोज',command=read,font=(None, sz)).grid(row=2,column=0)
-Button(root, text='सभी देखें',command=listall,font=(None, sz)).grid(row=2,column=1)
-Button(root, text='रक्षित करें',command=add,font=(None, sz)).grid(row=2,column=2)
+Button(root, text='खोज',command=read,font=(None, sz)).grid(row=3,column=0)
+Button(root, text='सभी देखें',command=listall,font=(None, sz)).grid(row=3,column=1)
+Button(root, text='रक्षित करें',command=add,font=(None, sz)).grid(row=3,column=2)
 EP=Button(root, text='अतिरिक्त निजीकरण',command=isa,font=(None, sz))
-EP.grid(row=4,column=1)
+EP.grid(row=5,column=1)
+Label(root, text="(c) Himanshu Attri\nv 1.10",font=(None, 5)).grid(row=5,column=2)
+
+Radiobutton(root, text="One", command=en(1), value=1).grid(row=6,column=1)
+
+
 #r=Tk()
 #app = App(master = r)
+
 mainloop()
 
 
